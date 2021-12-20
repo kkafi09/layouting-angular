a75e64ac969db436463241cc7568a114156ba565
unknown <afi.firda@gmail.com>
2021-12-20 10:18:52 +0700

    fic

--- src/app/pages/kasir/kasir.component.ts
+++ src/app/pages/kasir/kasir.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-kasir',
@@ -7,14 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KasirComponent implements OnInit {
   tableKasir = [
-    {id: 1, name:'Ayam goreng', price:'20000', qty:'2', discount:'0', total:'40000'},
-    {id: 2, name:'Nasi goreng', price:'20000', qty:'2', discount:'0', total:'40000'},
-    {id: 5, name:'Kopi', price:'20000', qty:'2', discount:'0', total:'40000'},
-    {id: 3, name:'Es Jeruk', price:'20000', qty:'2', discount:'0', total:'40000'},
-    {id: 4, name:'Teh', price:'20000', qty:'2', discount:'0', total:'40000'}
+    {id: 1, name: 'Ayam goreng', price: '20000', qty: '2', discount: '0', total: '40000'},
+    {id: 2, name: 'Nasi goreng', price: '20000', qty: '2', discount: '0', total: '40000'},
+    {id: 5, name: 'Kopi', price: '20000', qty: '2', discount: '0', total: '40000'},
+    {id: 3, name: 'Es Jeruk', price: '20000', qty: '2', discount: '0', total: '40000'},
+    {id: 4, name: 'Teh', price: '20000', qty: '2', discount: '0', total: '40000'}
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
