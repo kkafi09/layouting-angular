334a1cc47f67fec28be94cf94e07b6b58f4c93b5
unknown <afi.firda@gmail.com>
2021-12-24 10:02:12 +0700

    add export to pdf

--- src/app/pages/laporan/laporan.component.ts
+++ src/app/pages/laporan/laporan.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-laporan',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LaporanComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  download() {
+    const element = document.getElementById('table');
+    html2canvas(element).then((canvas) => {
+      console.log(canvas);
+      const imgWidth = 208;
+      const pageHeigh = 295;
+      const imgHeigh = canvas.height * imgWidth / canvas.width;
+      const heighLeft = imgHeigh;
+      const date = new Date();
+      const dtr = '' + date;
+
+      const imgData = canvas.toDataURL('image/png');
+
+      const doc = new jspdf.jsPDF('p', 'mm', 'a4');
+      const position = 20;
+      doc.setFontSize(8);
+      doc.text(dtr, 10, 10);
+      doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeigh);
+
+      doc.save('laporan.pdf');
+    });
   }
 
 }
